3db7545fc453daa6431f8d1e1b22840a336ad1cd
Sabin <spradh@users.noreply.github.com>
2017-04-06 17:24:24 -0400

    Create Missing letters.js

---
+++ Intermediate Algorithm Scripting/Missing letters.js
@@ -0,0 +1,18 @@
+
+function fearNotLetter(str) {
+  for(var i=0;i<str.length;i++){
+    if(i===0){
+      previous=str.charCodeAt(i);
+      continue;
+    }else{
+      if(previous==str.charCodeAt(i)-1){
+        previous=str.charCodeAt(i);
+      }else{
+        return String.fromCharCode(previous+1);
+      }
+    }
+  }
+  return undefined;
+}
+
+fearNotLetter("abce");
