1d9d0c675944e7827750ef263208b8b15e1338a5
Sabin <spradh@users.noreply.github.com>
2017-04-18 17:37:01 -0400

    Create Spinal Tap Case.js

---
+++ Intermediate Algorithm Scripting/Spinal Tap Case.js
@@ -0,0 +1,11 @@
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  var regex=/\s+|_+/g;
+  str=str.replace(/([a-z])([A-Z])/g,'$1 $2');
+  
+  return str.replace(regex,'-').toLowerCase();
+}
+
+spinalCase('This Is Spinal Tap');
